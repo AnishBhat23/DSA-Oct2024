6bd0bc0c0ac5851611a89246805a041ec24ffc5b
anish <emsonanish12@gmail.com>
2025-09-19 12:53:56 +0530

    V44 Sept19

---
+++ C++/ll_operations.cpp
@@ -0,0 +1,138 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+    public:
+    int data;
+    Node* next;
+
+    public:
+    Node(int data1, Node* next1)
+    {
+        data = data1;
+        next = next1;
+    }
+
+    public:
+    Node(int data1)
+    {
+        data = data1;
+        next = nullptr;
+    }
+};
+
+Node* convertArr2LL(vector<int> &arr)
+{
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+
+    for(int i=1; i<arr.size(); i++)
+    {
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        mover = mover->next;
+    }
+
+    return head;
+}
+
+void printLL(Node* head)
+{
+    Node* temp = head;
+    while(temp)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+
+Node* deleteHead(Node* head)
+{
+    Node* temp = head;
+
+    head = head->next;
+    free(temp);
+
+    return head;
+}
+
+Node* insertHead(Node* head, int data)
+{
+    Node* temp = new Node(data);
+
+    temp->next = head->next;
+    head = temp;
+
+    return head;
+}
+
+void deleteTail(Node* head)
+{
+    Node* temp = head;
+    while(temp->next != nullptr)
+    {
+        temp = temp->next;
+    }
+    free(temp->next);
+}
+
+Node* insertatPos(Node* head, int data, int pos)
+{
+    Node* temp = new Node(data);
+    Node* itr = head;
+    Node* ans = nullptr;
+
+    if(pos == 1)
+    {
+        ans = insertHead(head,data);
+        return ans;
+    }
+
+    int count = 1;
+    while(count < pos)
+    {
+        if(itr->next == nullptr)
+        {
+            break;
+        }
+        itr = itr->next;
+        count++;
+    }
+
+    if(itr->next != nullptr)
+    {
+        temp->next = itr->next;
+        itr->next = temp;
+    } 
+    else
+    {
+        itr->next = temp;
+    }
+    
+    ans = head;
+    return head;
+}
+
+Node* deleteAtPos(Node* head, int pos)
+{
+    
+}
+
+int main()
+{
+    vector<int> arr = {2, 5 ,8 ,7, 10};
+
+    Node* head = convertArr2LL(arr);
+
+    //deleteTail(head);
+
+    //insertatPos(head, 10, 5);
+    
+    insertatPos(head, 12, 5);
+
+    insertatPos(head, 15, 5);
+
+    printLL(head);
+
+}
